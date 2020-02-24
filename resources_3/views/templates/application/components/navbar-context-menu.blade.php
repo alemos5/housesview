62995fdb9958ba1e9636db91a944d8b488bfc4f5
Armando Lemos <josearmandomarcano@gmail.com>
2020-02-24 19:46:57 -0300

    remove file extra

--- resources_3/views/templates/application/components/navbar-context-menu.blade.php
+++
@@ -1,7 +0,0 @@
-<li class="nav-item">
-    <a class="nav-link context-menu-toggle waves-effect waves-dark"
-       href="javascript:void(0)"
-    >
-        <i class="ti-more-alt"></i>
-    </a>
-</li>
